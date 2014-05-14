72eb4c9d281b9c46c8fac619ad4c270e98baa96d
Prabha Pattabiraman <prabhap@thoughtworks.com>
2014-05-14 18:12:01 +0530

    Prabha - HEllo testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PrabhaTest::Application.config.secret_key_base = '12b9fd01b8099763fa1489086fb8eacaef1683875494a46c4d939804ccec8a848e5ffd9c6376d0e6b47b17c90ff2d327541929d80d5b496b087a66a3882e5265'
